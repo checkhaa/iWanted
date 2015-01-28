1deb8d7839f9ddd119e7c87f6b383d29edcc5c2f
Ahmet <ahmetguerer@googlemail.com>
2015-01-28 23:23:33 +0100

    update

--- www/js/core.js
+++ www/js/core.js
@@ -1,36 +1,29 @@
 $(document).ready(function(){
 	
 	$('#submenu-one').click(function(){	$('.submenu-one').toggle(300);	});
-	$('#submenu-two').click(function(){	$('.submenu-two').toggle(300);	});
-	
-	$('.deploy-left-sidebar, .close-sidebar-left, .close-bottom-left, .close-bottom-right').click(function(){	
+	$('#submenu-two').click(function(){	$('.submenu-two').toggle(300);	});    
+    
+	$('.deploy-left-sidebar, .close-sidebar-left, .header, .close-bottom-left, .close-bottom-right').click(function(){	
         return false;	
     });
-	
-    $(".content").click(function(){
+
+    
+    $(".deploy-left-sidebar, .header").click(function(){
+        $('.header').addClass('close_nav');
         $('.sidebar-left').animate({
-            left: '-270',
-        }, 300, 'easeOutExpo', function () {});
-        $('.sidebar-right').animate({
-            right: '-280px',
-        }, 300, 'easeInOutExpo', function () {});
-        return false;
-    });
-
-    $(".deploy-left-sidebar").click(function(){
-        $('.sidebar-left').delay(300).animate({
             left: '0',
-        }, 300, 'easeOutExpo', function () {});
+        }, 500, 'easeOutExpo', function () {});
         $('.sidebar-right').animate({
             right: '-280px',
-        }, 300, 'easeInOutExpo', function () {});
+        }, 500, 'easeInOutExpo', function () {});
         return false;
     });
 
-    $(".close-sidebar-left, .close-bottom-left, .content-box, .header").click(function(){        
+    $(".close-sidebar-left, .close-bottom-left, .container").click(function(){
+        $('.header').removeClass('close_nav');
         $('.sidebar-left').animate({
             left: '-270px',
-        }, 300, 'easeInOutExpo', function () {});
+        }, 500, 'easeInOutExpo', function () {});
         return false;
     });
     
@@ -42,11 +35,21 @@ $(document).ready(function(){
         
         $.post('http://wanted.setcode.de/scripts/login.php', dataString)
         .success(function(result){
+            // Wenn richtig eingeloggt Setze Storage und zeige die Maske nach dem erfolgreichem Login
             if(result == 'TRUE'){
                 var success_login = 'login_success';
                 window.localStorage.setItem('user_name', uname); 
                 window.localStorage.setItem('login', success_login);
-                window.location.href = "dashboard.html";
+                
+                // Login Maske ausblenden
+                $('div#loged_out').remove();
+                
+                // Eingeloggte Maske anzeigen
+                $('div#loged_in').fadeIn();
+                
+                // Sidebar laden
+                $('#loged_sidebar').load( "tpl/tpl.sidebar.html");
+                
             } else if(result == 'FALSE'){
                 $('#output').html('<div class="alert alert-danger"><strong>Benutzername oder Passwort falsch!</strong> Versuchen Sie es erneut</div>');   
             }
@@ -57,14 +60,3 @@ $(document).ready(function(){
         return false;
     });
 });
-
-
-
-
-
-
-
-
-
-
-
